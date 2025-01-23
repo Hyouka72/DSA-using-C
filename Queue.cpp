4f14d8ff5b2d0285efc79250ddb20328eb69f7dc
Hyouka45 <khewang212@gmail.com>
2025-01-24 00:11:12 +0545

    all done

--- Queue.cpp
+++ Queue.cpp
@@ -10,6 +10,7 @@ int n;
 public:
 queue(int size)         //construct || initialize of queue
 {
+    cout<<"Queue is Created"<<endl;
     arr= new int[size];
     front = -1;
     rear = -1;
@@ -17,7 +18,7 @@ queue(int size)         //construct || initialize of queue
 }
 
 void enqueue(int x){
-      if(rear == n-1){
+      if(rear == n-1){              //could have compare with n only but as rear start with -1 so we had to compare with n-1
         cout<<"The Queue is full"<<endl;
     }
     else{
@@ -59,11 +60,36 @@ void Display(){
 
 
 int main(){
-    queue q(1);
-    q.enqueue(1);
-    q.enqueue(2);
-    q.enqueue(2);
-    q.Display();
-
+    int size;
+    int in,n;
+    cout << "Basic Operation of Stack" << endl;
+    cout << "Enter the size of stack\n=>";
+    cin >> size;
+    queue q(size);
+        while (true)
+        {
+        cout << "\nEnter your choice\n 1 for enqueue\n 2 for dequeue\n 3 for display\n 4 for exit\n\n=>";  //Menu for the operation
+        cin >> n;
+        
+             switch (n) 
+             {
+            case 1:
+            cout<<"Enter the value to set in queue: ";                            //For enqeue operation
+            cin>>in;
+                q.enqueue(in);
+                break;
+            case 2:                                                                //For dequeue Operation
+               q.dequeue();
+                break;
+            case 3:                                                                 //For Display Operation
+                q.Display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+            }
+        }
+        
     return 0;
 }
